6ce8817b367605d5f8abbbd3ce238f4bb53c5e05
Rani0086 <shantidhaakad@gmail.com>
2025-01-16 16:06:38 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -5,7 +5,7 @@ class Config(object):
     OWNER_ID = "5886080930"
     sudo_users = "5886080930", "7526369190", "6961287189"
     GROUP_ID = -1002137208192
-    TOKEN = "8198269280:AAF9Kz2w34rzWgw2VjHedTOLEjH3k7Xq9bo"
+    TOKEN = "8198269280:AAF9Kz2w34rzWgdTOLEjH3k7Xq9bo"
     mongo_url = "mongodb+srv://I-LOVE-PDF-BOT:I-LOVE-PDF-BOT@cluster0.c51o3a9.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph/file/ed23556d07d33db18402d.jpg", "https://telegra.ph//file/e64337bbc6cdac7e6b178.jpg"]
     SUPPORT_CHAT = "kill_your_HW_Group"
